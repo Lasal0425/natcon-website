ddfdc1e9fb672027d499e182f6ebacfa28f53c6f
Lasal0425 <lasalrathnayake@gmail.com>
2026-04-23 04:01:05 +0530

    changes in the navbar

--- constants/NavBar.ts
+++ constants/NavBar.ts
@@ -1,8 +1,8 @@
 export const NAV_LINKS = [
   { name: "Home", href: "/" },
   { name: "About", href: "/#about" },
-  { name: "Partners", href: "/partners" },
   { name: "Title Partner", href: "/title-partner" },
+  { name: "Partners", href: "/partners" },
   { name: "Register", href: "/register" },
   { name: "Store", href: "/store" },
 ];
